e37c7cd4cac18261480aae497bc4cee602943564
Vlad Karpenko <karpenko.vlad10@gmail.com>
2018-08-04 20:12:48 +0300

    add WebSocketTask

---
+++ 19 - WebSocket/index.js
@@ -0,0 +1,23 @@
+const app = require('express')();
+const http = require('http').Server(app);
+const io = require('socket.io')(http);
+
+app.get('/', function (req, res) {
+    res.sendFile(__dirname + '/index.html');
+});
+
+http.listen(3000, function () {
+    console.log('listening on *:3000');
+});
+
+io.on('connection', function (socket) {
+    console.log('a user connected');
+
+    socket.on('disconnect', function(){
+        console.log('user disconnected');
+    });
+
+    socket.on('chat message', function(msg){
+        io.emit('chat message', msg);
+    });
+});
